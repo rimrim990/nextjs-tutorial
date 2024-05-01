df9622fce8168107160263fbb60133472edaa508
rimrim990 <lim990718@naver.com>
2024-05-01 23:42:35 +0900

    Chapter4. Creating Layouts and Pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+// 경로 진입점 설정
+export default function Page() {
+    return <p>Customers Page</p>
+}
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+// 여러 페이지 간 공유되는 공통 레이아웃 생성
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
+                <SideNav />
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+    );
+}
