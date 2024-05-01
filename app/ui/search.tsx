47fb15863f68d91f3d34526105090b1ff2884bc9
rimrim990 <lim990718@naver.com>
2024-05-02 02:05:12 +0900

    Chapter11. Adding Search and Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,48 @@
+// 이벤트 리스너와 훅 사용
 'use client';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {useDebouncedCallback} from "use-debounce";
 
-export default function Search({ placeholder }: { placeholder: string }) {
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
-  );
+export default function Search({placeholder}: { placeholder: string }) {
+    // 현재 URL 파라미터
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const {replace} = useRouter();
+
+    // 사용자가 타이핑을 300ms 이상 수행하지 않을 경우 코드가 실행됨
+    const handleSearch = useDebouncedCallback((term: string) =>  {
+        console.log(`Searching... ${term}`);
+
+        const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
+
+        if (term) {
+            params.set('query', term);
+        } else {
+            params.delete('query');
+        }
+
+        // URL 쿼리 값을 사용자가 입력한 데이터로 갱신
+        replace(`${pathname}?${params}`)
+    }, 300);
+
+    return (
+        <div className="relative flex flex-1 flex-shrink-0">
+            <label htmlFor="search" className="sr-only">
+                Search
+            </label>
+            <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                placeholder={placeholder}
+                onChange={(e) => {
+                    handleSearch(e.target.value);
+                }}
+                defaultValue={searchParams.get('query')?.toString()}
+            />
+            <MagnifyingGlassIcon
+                className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
+        </div>
+    );
 }
